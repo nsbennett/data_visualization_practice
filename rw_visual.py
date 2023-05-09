fe18cb825ac5b2b663d4b2d6ee0b1b11b7252475
Nathan Bennett <nathan@nathansbennett.com>
2023-05-08 23:57:48 -0500

    adjust size

--- rw_visual.py
+++ rw_visual.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9))
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors="none", s=1)
     ax.set_aspect('equal')
 
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
