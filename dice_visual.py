87d567f623d7c5446517fbb358bbb6848731fda5
Nathan Bennett <nathan@nathansbennett.com>
2023-05-10 23:42:53 -0500

    dice

--- dice_visual.py
+++ dice_visual.py
@@ -2,15 +2,18 @@ import plotly.express as px
 
 from die import Die
 
-die = Die()
+die_1 = Die()
+die_2 = Die()
+
 
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(1, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -23,7 +26,7 @@ for value in poss_results:
 
 
 
-title = "Results of Rolling One D6 1,000 Times"
+title = "Results of Rolling Two D6 1,000 Times"
 labels = {"x": "Result", "y": "Frequency of Result"}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.show()
