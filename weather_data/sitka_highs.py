17ef8a478bcb95d6f4155a2fc74a76d25c75986b
Nathan Bennett <nathan@nathansbennett.com>
2023-05-11 23:44:03 -0500

    weather data unit

---
+++ weather_data/sitka_highs.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import csv
+
+path = Path('sitka_weather_07-2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+    
+print(highs)
